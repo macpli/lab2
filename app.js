3561f57189a1decc07ce10db9a2ad04dbc277b49
macpli <pliszekmaciej0@gmail.com>
2024-10-17 23:23:31 +0200

    add pug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
